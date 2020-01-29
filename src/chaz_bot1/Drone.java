b375a34a2b573989b0ea47297bdb12d6970c30ac
charboltron <boltch@pdx.edu>
2020-01-28 23:49:36 -0800

    added take turn to drone

--- src/chaz_bot1/Drone.java
+++ src/chaz_bot1/Drone.java
@@ -1,9 +1,14 @@
 package chaz_bot1;
 
+import battlecode.common.GameActionException;
 import battlecode.common.RobotController;
 
 public class Drone extends Unit {
     public Drone(RobotController r) {
         super(r);
+    }
+
+    public void takeTurn() throws GameActionException{
+        super.takeTurn();
     }
 }
--- src/chaz_bot1/Refinery.java
+++ src/chaz_bot1/Refinery.java
@@ -16,7 +16,6 @@ public class Refinery extends Building {
         super.takeTurn();
 
 
-
         }
 
 }
