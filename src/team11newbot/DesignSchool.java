043298a5b48c98597eabf99ee5d7da8eefed5f91
sowmya <sowmya.sri.indukuri@gmail.com>
2020-01-29 22:59:56 -0800

    'netgunandfewunits'

--- src/team11newbot/DesignSchool.java
+++ src/team11newbot/DesignSchool.java
@@ -6,20 +6,24 @@ import battlecode.common.RobotController;
 import battlecode.common.RobotType;
 
 public class DesignSchool extends Building {
+    static int numLandscapers = 0;
     public DesignSchool(RobotController r) {
         super(r);
     }
 
     public void takeTurn() throws GameActionException {
         super.takeTurn();
-        
+
         // will only actually happen if we haven't already broadcasted the creation
         comms.broadcastDesignSchoolCreation(rc.getLocation());
 
-        for (Direction dir : Util.directions) {
-            if(tryBuild(RobotType.LANDSCAPER, dir)) {
+        if (numLandscapers < 4){
+            if(tryBuild(RobotType.LANDSCAPER, Util.randomDirection())) {
                 System.out.println("made a landscaper");
+                numLandscapers++;
             }
+
         }
+        else{}
     }
 }
