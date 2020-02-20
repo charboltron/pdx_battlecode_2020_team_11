2dee2f5bf3d416b1e98b9e6f6074bb0e302b330f
indukuri96 <sowmya.sri.indukuri@gmail.com>
2020-02-19 20:59:39 -0800

    '30%testcovearge'

---
+++ test/master_player/NavigationTest.java
@@ -0,0 +1,48 @@
+package master_player;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import org.junit.*;
+import org.mockito.*;
+import org.mockito.junit.*;
+
+public class NavigationTest {
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+    @Mock
+    RobotController rcMock = mock(RobotController.class);
+    @InjectMocks
+    Navigation navMock = new Navigation(rcMock);
+
+    @Before
+    public void setup() throws GameActionException {
+        when(rcMock.isReady()).thenReturn(true);
+        when(rcMock.canMove(Direction.CENTER)).thenReturn(true);
+        when(rcMock.getLocation()).thenReturn(new MapLocation(5, 5));
+        when(rcMock.senseFlooding(new MapLocation(5, 5))).thenReturn(false);
+    }
+    @Test
+    public void tryMove() throws GameActionException{
+        boolean result = navMock.tryMove(Direction.NORTH);
+        assertEquals(result,false);
+
+    }
+    @Test
+    public void droneMove() throws GameActionException{
+        boolean result = navMock.droneMove(Direction.CENTER);
+        assertEquals(true, result);
+
+    }
+    @Test
+    public void goTo() throws GameActionException{
+        boolean result = navMock.goTo(Direction.CENTER);
+        assertEquals(true, result);
+    }
+
+
+}
